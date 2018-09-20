b416f5fc56b7df591c5d9d721a36ede8b3985d0d
Hydrant <qbcbyb@sina.com>
2018-09-20 18:36:00 +0800

    初始化

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_gitlab_project_generater do
+  name 'Redmine Gitlab Project Generater plugin'
+  author 'Hydrant'
+  description 'This is a gitlab project generater plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/qbcbyb/redmine_gitlab_project_generater'
+  author_url 'https://github.com/qbcbyb'
+end
