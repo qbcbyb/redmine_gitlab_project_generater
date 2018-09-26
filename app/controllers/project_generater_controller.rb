4452fb5f3cbf430785d7087cc40bc80b486051a1
Hydrant <qbcbyb@sina.com>
2018-09-26 13:45:33 +0800

    加入了对原项目进行archive的操作

--- app/controllers/project_generater_controller.rb
+++ app/controllers/project_generater_controller.rb
@@ -45,7 +45,7 @@ class ProjectGeneraterController < ApplicationController
             success_urls.append http_url_to_repo
 
             source_project_namespace = ERB::Util.url_encode(source_path[1, source_path_dot_rindex - 1])
-            Net::HTTP.post(URI.join(source_remote_url, "/api/v#{source_gitlab_api_version}/projects/#{source_project_namespace}/archive?access_token=#{access_token}"), {})
+            Net::HTTP.post(URI.join(source_remote_url, "/api/v#{source_gitlab_api_version}/projects/#{source_project_namespace}/archive?access_token=#{access_token}"), '')
           rescue => ex
             error_projects.append new_project_name
             logger.error {"GitLab Project Generater: Project '#{new_project_name}' has error: #{ex.message}"}
